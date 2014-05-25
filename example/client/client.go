acd9ca740fe9f003d1b7d5a573e54dc715bac74d
Cihangir SAVAS <cihangir@koding.com>
2014-05-25 16:18:49 -0700

    Redisence: add more complex example for client application

--- example/client/client.go
+++ example/client/client.go
@@ -9,25 +9,53 @@ import (
 )
 
 func main() {
+	// if os.Getenv("GOMAXPROCS") == "" {
+	// 	runtime.GOMAXPROCS(runtime.NumCPU())
+	// }
+
 	session, err := redisence.New("localhost:6379", 10, time.Second*1)
 	if err != nil {
 		panic(err)
 	}
 
 	var wg sync.WaitGroup
-	wg.Add(2)
+	wg.Add(4)
 
 	go func() {
-		for i := 0; i < 10000; i++ {
-			session.Ping(strconv.Itoa(i))
-		}
+		pingdom(session, 1, 3000)
 		wg.Done()
 	}()
 	go func() {
-		for i := 0; i < 10000; i++ {
-			session.Status(strconv.Itoa(i))
-		}
+		pingdom(session, 3001, 6000)
 		wg.Done()
 	}()
+
+	go func() {
+		pingdom(session, 6001, 9000)
+		wg.Done()
+	}()
+
+	// go func() {
+	// 	for i := 0; i < 3333; i++ {
+	// 		session.Status(strconv.Itoa(i))
+	// 	}
+	// 	wg.Done()
+	// }()
+	wg.Done()
+
 	wg.Wait()
+}
+
+func pingdom(session *redisence.Session, start, end int) {
+	throttleCount := 1000
+	req := make([]string, throttleCount)
+	count := 0
+	for i := start; i <= end; i++ {
+		req[count] = strconv.Itoa(i)
+		if count == throttleCount-1 {
+			session.Ping(req...)
+			count = 0
+		}
+		count++
+	}
 }
