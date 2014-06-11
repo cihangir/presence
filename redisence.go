a1925f73e98321dacc25b5de3d514ac32ffeb6e2
Cihangir SAVAS <cihangir@koding.com>
2014-06-10 22:46:13 -0700

    Presence: remove redis backend from main file

--- redisence.go
+++ redisence.go
@@ -1,20 +1,6 @@
 // Package redisence provides simple user presence system
 package redisence
 
-import (
-	"errors"
-	"fmt"
-	"strconv"
-	"sync"
-	"time"
-
-	gredis "github.com/garyburd/redigo/redis"
-	"github.com/koding/redis"
-)
-
-// Prefix for redisence package
-const RedisencePrefix = "redisence"
-
 // Status defines what is the current status of a user
 // in presence system
 type Status int
@@ -43,327 +29,44 @@ type Event struct {
 	Status Status
 }
 
-// Session holds the required connection data for redis
 type Session struct {
-	// main redis connection
-	redis *redis.RedisSession
-
-	// inactiveDuration specifies no-probe allowance time
-	inactiveDuration time.Duration
-
-	// receiving offline events pattern
-	becameOfflinePattern string
-
-	// receiving online events pattern
-	becameOnlinePattern string
-
-	// errChan pipe all errors  the this channel
-	errChan chan error
-
-	// closed holds the status of connection
-	closed bool
-
-	//psc holds the pubsub channel if opened
-	psc *gredis.PubSubConn
-
-	// holds event channel
-	events chan Event
-
-	// lock for session struct
-	mu sync.Mutex
+	// holds the interface
+	backend Backend
 }
 
 // New creates a session for any broker system that is architected to use,
 // communicate, forward events to the presence system
-func New(server string, db int, inactiveDuration time.Duration) (*Session, error) {
-	redis, err := redis.NewRedisSession(&redis.RedisConf{Server: server, DB: db})
-	if err != nil {
-		return nil, err
-	}
-	redis.SetPrefix(RedisencePrefix)
-
-	return &Session{
-		redis:                redis,
-		becameOfflinePattern: fmt.Sprintf("__keyevent@%d__:expired", db),
-		becameOnlinePattern:  fmt.Sprintf("__keyevent@%d__:set", db),
-		inactiveDuration:     inactiveDuration,
-		errChan:              make(chan error, 1),
-	}, nil
+func New(backend Backend) (*Session, error) {
+	return &Session{backend: backend}, nil
 }
 
-// Close closes the redis connection gracefully
-func (s *Session) Close() error {
-	return s.close()
-}
-
-func (s *Session) close() error {
-	s.mu.Lock()
-	defer s.mu.Unlock()
-
-	if s.closed {
-		return errors.New("Closing of already closed connection")
-	}
-
-	s.closed = true
-
-	if s.events != nil {
-		close(s.events)
-	}
-
-	if s.psc != nil {
-		s.psc.PUnsubscribe()
-	}
-
-	return s.redis.Close()
-}
-
-// Error returns error if it happens while listening  to status changes
-func (s *Session) Error() error {
-	return <-s.errChan
-}
-
-// Ping resets the expiration time for any given key
-// if key doesnt exists, it means user is now online and should be set as online
-// Whenever application gets any prob from a client
-// should call this function
+// Online sets given ids as online, ignores any error
 func (s *Session) Online(ids ...string) error {
-	if len(ids) == 1 {
-		// if member exits increase ttl
-		if s.redis.Expire(ids[0], s.inactiveDuration) == nil {
-			return nil
-		}
-
-		// if member doesnt exist set it
-		return s.redis.Setex(ids[0], s.inactiveDuration, ids[0])
-	}
-
-	existance, err := s.sendMultiExpire(ids, s.inactiveDurationString())
-	if err != nil {
-		return err
-	}
-
-	return s.sendMultiSetIfRequired(ids, existance)
+	return s.backend.Online(ids...)
 }
 
 // Offline sets given ids as offline, ignores any error
-// since not exist keys returned as nilErr
 func (s *Session) Offline(ids ...string) error {
-	if len(ids) == 1 {
-		if s.redis.Expire(ids[0], time.Second*0) == nil {
-			return nil
-		}
-	}
-
-	s.sendMultiExpire(ids, "0")
-	return nil
-}
-
-// sendMultiSetIfRequired accepts set of ids and their existtance status
-// traverse over them and any key is not exists in db, set them in a multi/exec
-// request
-func (s *Session) sendMultiSetIfRequired(ids []string, existance []int) error {
-	if len(ids) != len(existance) {
-		return fmt.Errorf("Length is not same Ids: %d Existance: %d", len(ids), len(existance))
-	}
-
-	// cache inactive duration
-	seconds := s.inactiveDurationString()
-
-	// get one connection from pool
-	c := s.redis.Pool().Get()
-
-	// item count for non-existent members
-	notExistsCount := 0
-
-	for i, exists := range existance {
-		// `0` means, member doesnt exists in presence system
-		if exists != 0 {
-			continue
-		}
-
-		// init multi command lazily
-		if notExistsCount == 0 {
-			c.Send("MULTI")
-		}
-
-		notExistsCount++
-		c.Send("SETEX", s.redis.AddPrefix(ids[i]), seconds, ids[i])
-	}
-
-	// execute multi command if only we flushed some to connection
-	if notExistsCount != 0 {
-		// ignore values
-		if _, err := c.Do("EXEC"); err != nil {
-			return err
-		}
-	}
-
-	// do not forget to close the connection
-	if err := c.Close(); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-// sendMultiExpire if the system tries to update more than one key at a time
-// inorder to leverage rtt, send multi expire
-func (s *Session) sendMultiExpire(ids []string, duration string) ([]int, error) {
-	// get one connection from pool
-	c := s.redis.Pool().Get()
-
-	// init multi command
-	c.Send("MULTI")
-
-	// send expire command for all members
-	for _, id := range ids {
-		c.Send("EXPIRE", s.redis.AddPrefix(id), duration)
-	}
-
-	// execute command
-	r, err := c.Do("EXEC")
-	if err != nil {
-		return nil, err
-	}
-
-	// close connection
-	if err := c.Close(); err != nil {
-		return nil, err
-	}
-
-	values, err := s.redis.Values(r)
-	if err != nil {
-		return nil, err
-	}
-
-	res := make([]int, len(values))
-	for i, value := range values {
-		res[i], err = s.redis.Int(value)
-		if err != nil {
-			// what about returning half-generated slice?
-			// instead of an empty one
-			return nil, err
-		}
-
-	}
-
-	return res, nil
+	return s.backend.Offline(ids...)
 }
 
 // MultipleStatus returns the current status multiple keys from system
 func (s *Session) Status(ids ...string) ([]Event, error) {
-	if len(ids) == 1 {
-		return s.singleStatus(ids[0])
-	}
-
-	// get one connection from pool
-	c := s.redis.Pool().Get()
-
-	// init multi command
-	c.Send("MULTI")
-
-	// send expire command for all members
-	for _, id := range ids {
-		c.Send("EXISTS", s.redis.AddPrefix(id))
-	}
-
-	// execute command
-	r, err := c.Do("EXEC")
-	if err != nil {
-		return nil, err
-	}
-
-	// close connection
-	if err := c.Close(); err != nil {
-		return nil, err
-	}
-
-	values, err := s.redis.Values(r)
-	if err != nil {
-		return nil, err
-	}
-
-	res := make([]Event, len(values))
-	for i, value := range values {
-		status, err := s.redis.Int(value)
-		if err != nil {
-			return nil, err
-		}
-
-		res[i] = Event{
-			Id: ids[i],
-			// cast redis response to Status
-			Status: Status(status),
-		}
-	}
-
-	return res, nil
+	return s.backend.Status(ids...)
 }
 
-// Status returns the current status a key from system
-func (s *Session) singleStatus(id string) ([]Event, error) {
-	res := make([]Event, 1)
-	res[0] = Event{
-		Id:     id,
-		Status: Offline,
-	}
+// Close closes the redis connection gracefully
+func (s *Session) Close() error {
+	return s.backend.Close()
+}
 
-	if s.redis.Exists(id) {
-		res[0].Status = Online
-	}
-
-	return res, nil
+// Error returns error if it happens while listening  to status changes
+func (s *Session) Error() error {
+	return s.backend.Error()
 }
 
 // ListenStatusChanges subscribes with a pattern to the redis and
 // gets online and offline status changes from it
 func (s *Session) ListenStatusChanges() chan Event {
-	s.psc = s.redis.CreatePubSubConn()
-	s.psc.PSubscribe(s.becameOnlinePattern, s.becameOfflinePattern)
-
-	s.events = make(chan Event)
-	go s.listenEvents()
-	return s.events
-}
-
-// createEvent Creates the event with the required properties
-func (s *Session) listenEvents() {
-	for {
-		s.mu.Lock()
-		if s.closed {
-			s.mu.Unlock()
-			return
-		}
-		s.mu.Unlock()
-
-		switch n := s.psc.Receive().(type) {
-		case gredis.PMessage:
-			s.events <- s.createEvent(n)
-		case error:
-			s.errChan <- n
-			return
-		}
-	}
-}
-
-// createEvent Creates the event with the required properties
-func (s *Session) createEvent(n gredis.PMessage) Event {
-	e := Event{}
-
-	switch n.Pattern {
-	case s.becameOfflinePattern:
-		e.Id = string(n.Data[len(RedisencePrefix)+1:])
-		e.Status = Offline
-	case s.becameOnlinePattern:
-		e.Id = string(n.Data[len(RedisencePrefix)+1:])
-		e.Status = Online
-	default:
-		//ignore other events
-	}
-
-	return e
-}
-
-func (s *Session) inactiveDurationString() string {
-	return strconv.Itoa(int(s.inactiveDuration.Seconds()))
+	return s.backend.ListenStatusChanges()
 }
