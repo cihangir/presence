ca75dafd598bdcb7a29c8c49053d5a33168b60ab
Cihangir SAVAS <cihangir@koding.com>
2014-06-10 22:46:41 -0700

    Presence: fix test creation process after interface refactoring

--- redisence_test.go
+++ redisence_test.go
@@ -8,10 +8,13 @@ import (
 )
 
 func initRedisence(t *testing.T) *Session {
-	ses, err := New("localhost:6379", 10, time.Second*1)
+
+	backend, err := NewRedis("localhost:6379", 10, time.Second*1)
 	if err != nil {
 		t.Fatal(err)
 	}
+
+	ses := New(backend)
 	conn := ses.redis.Pool().Get()
 	conn.Do("CONFIG", "SET", "notify-keyspace-events Ex$")
 	if err := conn.Close(); err != nil {
