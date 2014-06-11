78101bb1c217178afc88bb8340c3b71062fe1c08
Cihangir SAVAS <cihangir@koding.com>
2014-06-10 23:44:17 -0700

    Presence: create backed with NewRedis function

--- example/client/client.go
+++ example/client/client.go
@@ -16,7 +16,7 @@ func main() {
 		runtime.GOMAXPROCS(runtime.NumCPU())
 	}
 
-	backend, err := redisence.New("localhost:6379", 10, time.Second*1)
+	backend, err := redisence.NewRedis("localhost:6379", 10, time.Second*1)
 	if err != nil {
 		panic(err)
 	}
